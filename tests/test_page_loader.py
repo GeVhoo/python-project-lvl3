f5e3ce6c0c1aadf9d45141268f515aa8fdc7bbbd
GeVhoo <gvahrushev@mail.ru>
2020-02-28 15:36:06 +0500

    Add some tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,11 +1,25 @@
 from page_loader import getter
 
-PATH = 'https://hexlet.io/courses'
+URL = 'https://hexlet.io/courses'
 HTML_FILE_NAME = 'hexlet-io-courses.html'
 FOLDER_NAME = 'hexlet-io-courses_files'
 BASE_URL = 'https://hexlet.io'
+RESOURCE_TRUE = '/assets/application.css'
+RESOURCE_FALSE = 'https://mc.yandex.ru/metrika/tag.js'
+RESOURCE_URL = 'https://hexlet.io/assets/application.css'
+PATH = '/home/tests'
+RESOURCE_PATH = '/home/tests/assets-application.css'
 BASE = BASE_URL, HTML_FILE_NAME, FOLDER_NAME
 
 
-def test_result():
-    assert BASE == getter.get_base_variables(PATH)
+def test_base_variables():
+    assert BASE == getter.get_base_variables(URL)
+
+
+def test_resource_url():
+    assert RESOURCE_URL == getter.get_resource_url(BASE_URL, RESOURCE_TRUE)
+    assert getter.get_resource_url(BASE_URL, RESOURCE_FALSE) is None
+
+
+def test_resource_path():
+    assert RESOURCE_PATH == getter.get_resource_path(PATH, RESOURCE_TRUE)
