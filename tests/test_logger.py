aafa5060ced758b9a33710ffd33f8536b4da5415
GeVhoo <gvahrushev@mail.ru>
2020-04-11 13:30:42 +0500

    Update test_logger

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,9 +2,8 @@ import logging
 
 from page_loader import logger
 
-test_logger = logging.getLogger()
-
 
 def test_set_logger_debug():
+    test_logger = logging.getLogger()
     logger.set_logger('debug')
     assert logging.Logger.getEffectiveLevel(test_logger) == logging.getLevelName('DEBUG')  # noqa: E501
